afefa3e90e1d37dce2b5faff44571ed434b4736b
Jaime Flandes <142937442+haifly32@users.noreply.github.com>
2026-04-27 16:39:32 -0400

    Fix typo in AdoptionApplication table definition

--- CreateTables.sql
+++ CreateTables.sql
@@ -37,7 +37,7 @@ CREATE TABLE Staff (
 
 CREATE TABLE AdoptionApplication (
     application_id INTEGER AUTO_INCREMENT NOT NULL,
-    pplication_date DATE NOT NULL,
+    application_date DATE NOT NULL,
     application_status ENUM('Pending', 'Approved', 'Rejected') NOT NULL,
     pet_id INTEGER NOT NULL,
     PRIMARY KEY (application_id),
